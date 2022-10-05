e47e3636fa032d951623a5d9f11a5f05549d76fa
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-10-05 14:54:34 +0100

    Creating a count app, using React hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component, useState } from 'react';
 
-class App extends Component {
-  state = {
-    count: 0
-  };
+const App = () => {
+    const [count, setCount] = useState(0);
 
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  }
-    render() {
-      return (
-      <div>
-        <h2>Counter app</h2>
-        <button onClick={this.increment}> 
-          Clicked {this.state.count} times.</button>
-      </div>
-      );
-    }
+    const increment  = () => {
+        setCount(count +1);
+    };
+
+    return (
+        <div>
+            <h2>Counter app</h2>
+                <button onClick={increment}>Clicked {count} times</button>
+        </div>
+     );
 };
+// class App extends Component {
+//   state = {
+//     count: 0
+//   };
+
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count + 1
+//     });
+//   }
+//     render() {
+//       return (
+//       <div>
+//         <h2>Counter app</h2>
+//         <button onClick={this.increment}> 
+//           Clicked {this.state.count} times.</button>
+//       </div>
+//       );
+//     }
+// };
 
 export default App;
